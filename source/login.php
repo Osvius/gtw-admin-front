827fbec84274226b4a2777dca22dfdf95e8788aa
Gudymenko Oleg <pwnz1234565@gmail.com>
2017-06-27 14:58:53 +0300

    login page

---
+++ source/login.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<?php include('head.php')?>
+<body>
+<div id="wrapper" class="login_page">
+	<header class="login_header">
+		<div class="logo">
+			<span class="logo_item1">goto</span>
+			<span class="logo_item2">web</span>
+		</div>
+	</header>
+	<div class="login_wrap">
+		<h1 class="login_greeting">Добро пожаловать в Go To Web админ панель</h1>
+		<div class="login_container">
+			<div class="login_head"><p>Введите логин и пароль</p></div>
+			<div class="login_content">
+				<div class="inputs_wrap">
+					<label class="label" for="login">Логин</label>
+					<div class="input_block">
+						<span class="input_img user_img"></span>
+						<input type="text" class="input" name="name" id="login" >
+					</div>
+				</div>
+				<div class="inputs_wrap">
+					<label class="label" for="password">Пароль</label>
+					<div class="input_block">
+						<span class="input_img user_key"></span>
+						<input type="password" class="input" name="name" id="password" >
+					</div>
+					<span class="help-block">
+						<a href="#" >Забыли пароль?</a>
+					</span>	
+				</div>
+				<div class="btn_block">
+					<button class="btn_enter">Войти</button>
+				</div>
+			</div>
+		</div>
+	</div>	
+		
+</div>
+
+</body>
+</html>
