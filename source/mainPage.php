e6c8bcce509209f103e2ffc3d49c1b234050da48
Gudymenko Oleg <pwnz1234565@gmail.com>
2017-07-07 15:59:08 +0300

    main page

--- source/mainPage.php
+++ source/mainPage.php
@@ -6,42 +6,42 @@
 <main id="main">
 	  <div class="content-wrap">
 	  <?php include('aside.php')?>
-		<div class="order_wrap">
-			<h5>Здесь Вы можете настроить свой магазин.</h5>
+			<div class="order_wrap">
+			<h5></h5>
 			<div class="order_content">
 				
 				<div class="main_form">
 		
 				<div class="order_head">
-					<span class="order_list_name">
-					 <!-- Всего два простых шага -->
-					</span>
+					<h2 class="head_text">
+					 Здесь Вы можете настроить свой магазин
+					</h2>
 	
 				</div>
 				
 				<form action="post" class="main_page" >
-					
+
 					<a href="#" class="main_item item1">
-						<span class="number">1</span>
+						<span class="number">01</span>
+						<span class="icon"></span>
+						<div class="item_content">
+							<img src="image/gtw-images/monitor.png" alt="shop_sett">
+							<p>Настройки магазина</p>
+						</div>
+					</a>					
+					<a href="#" class="main_item item2">
+						<span class="number">02</span>
 						<span class="icon"></span>
 						<div class="item_content">
 							<img src="image/gtw-images/online-shop.png" alt="">
-							<p>Добаление товара</p>
-						</div>
-					</a>
-					<a href="#" class="main_item item2">
-						<span class="number">2</span>
-						<span class="icon"></span>
-						<div class="item_content">
-							<img src="image/gtw-images/folder.png" alt="">
-							<p>Настройки магазина</p>
+							<p>Добавление товара</p>
 						</div>
 					</a>
 
 				</form>
 
 			</div>
-		</div>
+		</div>	
 	  </div>
 </main>
 
