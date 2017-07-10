c420c63f2541e4c837572bb3fe188b5d3a48eadf
Gudymenko Oleg <pwnz1234565@gmail.com>
2017-07-10 13:47:43 +0300

    aside

--- source/aside.php
+++ source/aside.php
@@ -2,58 +2,63 @@
 			<h2>Панель&nbsp;управления</h2>
 			<div class="aside_content">
 				<h3>Доступные разделы</h3>
-								<ul class="aside_nav">
-					<li>
+				<ul class="aside_nav">
+					<li class="active">
 						<a href="#">
 							<span class="item-icon real-estate"></span>
 							<span class="item-name">Мой магазин</span>
 						</a>
-					</li>
+					</li>				
 					<li>
 						<a href="#">
 							<span class="item-icon slider"></span>
 							<span class="item-name">Слайдер</span>
 						</a>
 					<li>
-						<a href="#">
-							<span class="item-icon list"></span>
-							<span class="item-name">Категории</span>
-						</a>
-					</li>
 					<li> 
 						<a href="#">
 							<span class="item-icon portfolio"></span>
 							<span class="item-name">Товары</span>
 						</a>
-					</li>
+					</li>					
 					<li>
 						<a href="#">
-							<span class="item-icon factory"></span>
-							<span class="item-name">Производители</span>
+							<span class="item-icon real-estate"></span>
+							<span class="item-name">Расширенный настройки</span>
 						</a>
+						<ul class="sub-nav hidden">
+							<li>
+								<a href="#">
+									<span class="item-icon list"></span>
+									<span class="item-name">Категории</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="item-icon factory"></span>
+									<span class="item-name">Производители</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="item-icon folder2"></span>
+									<span class="item-name">Характеристики</span>
+								</a>
+							</li>							
+
+						</ul>
 					</li>
-					<li>
+					<li >
 						<a href="#">
-							<span class="item-icon folder1"></span>
-							<span class="item-name">Группы Характеристик</span>
+							<span class="item-icon attach"></span>
+							<span class="item-name">Заказы</span>
 						</a>
 					</li>
-					<li>
-						<a href="#">
-							<span class="item-icon folder2"></span>
-							<span class="item-name">Характеристики</span>
-						</a>
-					</li>
+
 					<li>
 						<a href="#">
 							<span class="item-icon truck"></span>
 							<span class="item-name">Доставка / Оплата</span>
-						</a>
-					</li>
-					<li class="active">
-						<a href="#">
-							<span class="item-icon attach"></span>
-							<span class="item-name">Заказы</span>
 						</a>
 					</li>
 					<li >
@@ -61,7 +66,7 @@
 							<span class="item-icon gallery"></span>
 							<span class="item-name">Галерея</span>
 						</a>
-					</li>					
+					</li>	
 					
 				</ul>
 			</div>
